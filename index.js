ebc5e22c4bce1ad1c9ee1be7777399e91e5a8ed3
granteckels <geckels1@gmail.com>
2025-02-08 19:08:33 -0600

    Add writing to README.md file

--- index.js
+++ index.js
@@ -1,36 +1,37 @@
 // TODO: Include packages needed for this application
 import inquirer from "inquirer";
+import fs from 'fs';
 
 // TODO: Create an array of questions for user input
 const questions = [
   // type: 'input' is default
   {
-    name: 'title',
+    name: 'Title',
     message: 'Enter the project title:'
   },
   {
-    name: 'description',
+    name: 'Description',
     message: 'Enter the project description:'
   },
   {
-    name: 'installation',
+    name: 'Installation',
     message: 'Enter the installation instructions:'
   },
   {
-    name: 'usage',
+    name: 'Usage',
     message: 'Enter the usage information:'
   },
   {
-    name: 'contribution',
+    name: 'Contributing',
     message: 'Enter instructions about how to contribute:'
   },
   {
-    name: 'test',
+    name: 'Tests',
     message: 'Enter the instructions to test the application:'
   },
   {
     type: 'list',
-    name: 'license',
+    name: 'License',
     message: 'Select license to use:',
     choices: [
       'Apache License 2.0',
@@ -51,14 +52,65 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  // Rearrange the data so it's in the correct order
+  const keyOrder = [
+    "Title",
+    "Description",
+    "Installation",
+    "Usage",
+    "License",
+    "Contributing",
+    "Tests",
+    "githubUsername",
+    "email"
+  ]
+
+  data = Object.fromEntries(
+    keyOrder.map(key => [key, data[key]])
+  )
+
+  let doc = '';
+
+  for(const section of Object.keys(data).slice(0,-2)) {
+    if(section === 'Title') {
+      doc = `# ${data[section]}\n\n`;
+    } else {
+      doc += `## ${section}\n\n${data[section]}\n\n`;
+      
+      if(section === 'Description') {
+        // Add Table of Contents after Description
+        doc += '## Table of Contents\n\n';
+        doc += '- [Installation](#installation)\n\n';
+        doc += '- [Usage](#usage)\n\n';
+        doc += '- [License](#license)\n\n';
+        doc += '- [Contributing](#contributing)\n\n';
+        doc += '- [Tests](#tests)\n\n';
+        doc += '- [Questions](#questions)\n\n';
+      }
+    }
+  }
+
+  doc += `## Questions\n\n`;
+  doc += `https://github.com/${data['githubUsername']}\n\n`;
+  doc += `For additional questions contact ${data['email']}`;
+
+  fs.writeFile(fileName, doc, "utf8", (err) => {
+    if(err) console.log(err);
+  })
+}
 
 // TODO: Create a function to initialize app
 function init() {}
 
+import { data } from './testData.js';
+
+writeToFile('README.md', data);
+
 // Function call to initialize app
 init();
 
-inquirer.prompt(questions).then((answers) => {
-  console.log(JSON.stringify(answers, null, '  '));
-})
+// inquirer.prompt(questions).then((answers) => {
+//   console.log(JSON.stringify(answers, null, '  '));
+//   writeToFile('README.md', answers);
+// })
---
+++ testData.js
@@ -0,0 +1,20 @@
+export const data = {
+    "Title": "Foodies Delight",
+    "Description": "As disorganized cooking enthusiasts, we want an application that stores all of our favorite recipes in one central location. Deciding what to eat everyday, remembering all the ingredients and the cooking instruction s is too stressful. We wanted a simple streamlined application that houses our recipes and that we can access from the web.",
+    "Installation": "Go to site https://programwithkimta.github.io/PROJECT-REPO/",
+    "Usage": "Enter the title, ingredients, and instructions for the recipe in the respective fields in the form on the right, and click Submit. It will be added in the center of the page. To delete all recipes, click the Delete button underneath 'Your Favorite Recipe' header. Recipes will be stored locally and be persistent on the application page.",
+    "Contributing": "Please add unit tests for every change and documentation for every API change. Also feel free to submit typo fixes and documentation improvements.",
+    "Tests": "Run `npm test` to run unit tests.",
+    "License": "MIT",
+    "githubUsername": "granteckels",
+    "email": "geckels1@gmail.com"
+}
+
+// let foo = 
+// `one` + `
+// two
+// `
+
+// foo +=
+// `three`
+// console.log(foo);
